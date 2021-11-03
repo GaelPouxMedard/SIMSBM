49dca33daf0ec6354317fbbbb0c55f8530932774
GaelPouxMedard <gael.poux-medard@univ-lyon2.fr>
2021-11-03 19:45:33 +0100

    Changes

--- nMMSBM.py
+++ nMMSBM.py
@@ -832,10 +832,11 @@ else:  # EXPERIMENTAL SETUP
             list_params.append(([2, 3], [1, 2], [1, 1], [10, 10], False, 0))
             list_params.append(([2, 3], [1, 2], [1, 2], [10, 10], False, 0))
 
-            list_params.append(([1, 2, 3], [1, 1, 1], [1, 1, 1], [10, 10, 10], False, 0))
-
             list_params.append(([1, 3], [1, 2], [1, 1], [10, 10], False, 0))  # Maybe too large
             list_params.append(([1, 3], [1, 2], [1, 2], [10, 10], False, 0))
+
+            list_params.append(([1, 2, 3], [1, 1, 1], [1, 1, 1], [10, 10, 10], False, 0))
+
         if "drugs" in folder.lower():
             # 0 = drugs, 1 = age, 2 = gender, 3 = education  ;  o = attitude (NotSensationSeeking, Introvert, Closed, Calm, Unpleasant, Unconcious, NonNeurotics)
             list_params = []
@@ -843,23 +844,25 @@ else:  # EXPERIMENTAL SETUP
             list_params.append(([0], [3], [2], [7], False, 0))
             list_params.append(([0], [3], [3], [7], False, 0))
 
+            list_params.append(([0, 3], [3, 1], [1, 1], [7, 5], False, 0))
+            list_params.append(([0, 3], [3, 1], [2, 1], [7, 5], False, 0))
+            list_params.append(([0, 3], [3, 1], [3, 1], [7, 5], False, 0))
+
             list_params.append(([0, 1, 2, 3], [3, 1, 1, 1], [1, 1, 1, 1], [7, 3, 3, 5], False, 0))
             list_params.append(([0, 1, 2, 3], [3, 1, 1, 1], [2, 1, 1, 1], [7, 3, 3, 5], False, 0))
             list_params.append(([0, 1, 2, 3], [3, 1, 1, 1], [3, 1, 1, 1], [7, 3, 3, 5], False, 0))
 
-            list_params.append(([0, 3], [3, 1], [1, 1], [7, 5], False, 0))
-            list_params.append(([0, 3], [3, 1], [2, 1], [7, 5], False, 0))
-            list_params.append(([0, 3], [3, 1], [3, 1], [7, 5], False, 0))
         if "mrbanks" in folder.lower():
             # 0 = usr, 1 = situation, 2 = gender, 3 = age, 4=key  ;  o = decision (up/down)
             list_params = []
+            list_params.append(([0, 1], [1, 3], [1, 1], [5, 5], False, 0))
+            list_params.append(([0, 1], [1, 3], [1, 2], [5, 5], False, 0))
+            list_params.append(([0, 1], [1, 3], [1, 3], [5, 5], False, 0))
+
             list_params.append(([0, 1, 2, 3], [1, 3, 1, 1], [1, 1, 1, 1], [5, 5, 3, 3], False, 0))
             list_params.append(([0, 1, 2, 3], [1, 3, 1, 1], [1, 2, 1, 1], [5, 5, 3, 3], False, 0))
             list_params.append(([0, 1, 2, 3], [1, 3, 1, 1], [1, 3, 1, 1], [5, 5, 3, 3], False, 0))
 
-            list_params.append(([0, 1], [1, 3], [1, 1], [5, 5], False, 0))
-            list_params.append(([0, 1], [1, 3], [1, 2], [5, 5], False, 0))
-            list_params.append(([0, 1], [1, 3], [1, 3], [5, 5], False, 0))
     except Exception as e:
         print(e)
         pass
