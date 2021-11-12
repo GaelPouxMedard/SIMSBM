9c18bdd5394e476055dea973ef6fe043af63e36c
GaelPouxMedard <61467946+GaelPouxMedard@users.noreply.github.com>
2021-11-12 11:06:14 +0100

    Changes

--- treatDataTwitter.py
+++ treatDataTwitter.py
@@ -374,8 +374,8 @@ def retreat(folder):
 def treatForNMMSBM():
     nbOccTweet = {}
     nbOccRetweet = {}
-    thresInput = 1
-    thresOutput = 1
+    thresInput = 20
+    thresOutput = 20
     with open("Data/Twitter/nounsPost.txt", "r", encoding="utf-8") as f_raw:
         with open("Data/Twitter/outcome.txt", "a", encoding="utf-8") as o:
             o.truncate(0)
