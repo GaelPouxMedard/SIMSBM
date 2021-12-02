a0bd7e74724da66fea448fe9f6cedde97b0b68d9
GaelPouxMedard <61467946+GaelPouxMedard@users.noreply.github.com>
2021-12-02 12:09:10 +0100

    Changes

--- nMMSBM.py
+++ nMMSBM.py
@@ -893,7 +893,8 @@ else:  # EXPERIMENTAL SETUP
         if "drugs" in folder.lower():
             # 0 = drugs, 1 = age, 2 = gender, 3 = education  ;  o = attitude (NotSensationSeeking, Introvert, Closed, Calm, Unpleasant, Unconcious, NonNeurotics)
             list_params = []
-            list_params.append(([0], [3], [1], [7], False, 0))
+            list_params.append(([0], [3], [1], [7], True, 0))
+            '''
             list_params.append(([0], [3], [2], [7], False, 0))
             list_params.append(([0], [3], [3], [7], False, 0))
 
@@ -904,6 +905,7 @@ else:  # EXPERIMENTAL SETUP
             list_params.append(([0, 1, 2, 3], [3, 1, 1, 1], [1, 1, 1, 1], [7, 3, 3, 5], False, 0))
             list_params.append(([0, 1, 2, 3], [3, 1, 1, 1], [2, 1, 1, 1], [7, 3, 3, 5], False, 0))
             list_params.append(([0, 1, 2, 3], [3, 1, 1, 1], [3, 1, 1, 1], [7, 3, 3, 5], False, 0))
+            '''
         if "mrbanks" in folder.lower():
             # 0 = usr, 1 = situation, 2 = gender, 3 = age, 4=key  ;  o = decision (up/down)
             list_params = []
